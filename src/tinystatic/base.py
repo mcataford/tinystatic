e8292a6a65aa1ceb923a73530875d216be601a8f
Marc Cataford <mcat@riseup.net>
2021-12-08 19:00:02 -0500

    fix: parser args

--- src/tinystatic/base.py
+++ src/tinystatic/base.py
@@ -13,24 +13,23 @@ def tinystatic():
     """
 
     parser = argparse.ArgumentParser(
-        description="Generate quick static websites from markdown."
+        description="Generate quick static websites from markdown.", add_help=False
     )
-    subparsers = parser.add_subparsers()
-
-    # Build
-    build_subparser = subparsers.add_parser("build")
-    build_subparser.set_defaults(command="build")
-
-    # Init command
-    init_subparser = subparsers.add_parser("init")
-    init_subparser.set_defaults(command="init")
-
     parser.add_argument(
         "--cwd",
         type=Path,
         default=str(Path.cwd()),
         help="Project root (current directory if not specified)",
     )
+    subparsers = parser.add_subparsers()
+
+    # Build
+    build_subparser = subparsers.add_parser("build", parents=[parser])
+    build_subparser.set_defaults(command="build")
+
+    # Init command
+    init_subparser = subparsers.add_parser("init", parents=[parser])
+    init_subparser.set_defaults(command="init")
 
     args = parser.parse_args()
 
