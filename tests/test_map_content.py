03054005350da31d59f32a53b52df5bffb3c46aa
Marc Cataford <mcat@riseup.net>
2021-12-10 19:47:43 -0500

    wip: temp log out test vars

--- tests/test_map_content.py
+++ tests/test_map_content.py
@@ -77,5 +77,5 @@ def test_map_content_includes_configured_keys(tmpdir, patched_cli_args):
 
     mock_step_args = mocked_step.call_args.args[0]
     
-    print(mocked_step.call_args)
+    print(mocked_step.call_args.args)
     assert mock_step_args["content_map"] == {"post.md": {"key1": "a", "key2": "b"}}
