c027f99cc21d0345dfe3f7c84fdee2a15e16910d
Marc Cataford <mcat@riseup.net>
2021-12-11 16:16:32 -0500

    refactor: always populate content_map

--- src/tinystatic/steps/map_content.py
+++ src/tinystatic/steps/map_content.py
@@ -1,3 +1,17 @@
+"""
+The content mapping step crawls available content (as defined by paths.content_path)
+and extracts metadata from the frontmatter of each content item. The keys extracted
+from the frontmatter are defined by setting steps.map_content.include in the
+configuration.
+
+Depends on:
+    stash.cwd
+    stash.config.path.content_path
+    stash.config.steps.map_content.include
+
+The results of this step are written to stash.content_map in the shared stash.
+"""
+
 from pathlib import Path
 from typing import List
 
@@ -33,14 +47,12 @@ def run(stash):
         stash["config"].get("steps", {}).get("map_content", {}).get("include")
     )
 
-    if not included_metadata:
-        return
-
     content_map = {}
 
-    for item_path in get_content_item_paths(full_content_path):
-        content_map[str(item_path.relative_to(full_content_path))] = get_metadata(
-            item_path, included_metadata
-        )
+    if included_metadata:
+        for item_path in get_content_item_paths(full_content_path):
+            content_map[str(item_path.relative_to(full_content_path))] = get_metadata(
+                item_path, included_metadata
+            )
 
     stash["content_map"] = content_map
